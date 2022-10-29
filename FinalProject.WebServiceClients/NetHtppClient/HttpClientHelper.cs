413808114a0c88f6179d9016565e8e9b74391e54
Joshua Naval <joshua.naval@softvision.com>
2022-10-30 01:39:34 +0800

    Add Web Service Clients Project

---
+++ FinalProject.WebServiceClients/NetHtppClient/HttpClientHelper.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace FinalProject.WebServiceClients.NetHttpClient
+{
+    public class HttpClientHelper
+    {
+        public Dictionary<string, string> RequestHeaders { get; set; }
+
+        public Dictionary<string, string> RequestOptions { get; set; }
+
+        public HttpResponseMessage Response { get; set; }
+
+        private static readonly HttpClient Client = new();
+
+        private readonly string _baseUrl;
+
+        public HttpClientHelper(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public void AddRequestHeaders(string key, string value)
+        {
+            RequestHeaders ??= new Dictionary<string, string>();
+
+            RequestHeaders.Add(key, value);
+        }
+
+        public void AddRequestOptions(string key, string value)
+        {
+            RequestOptions ??= new Dictionary<string, string>();
+
+            RequestOptions.Add(key, value);
+        }
+
+        private HttpRequestMessage SetRequestHeaders(HttpRequestMessage requestMessage)
+        {
+            foreach (var requestHeader in RequestHeaders!)
+            {
+                requestMessage.Headers.Add(requestHeader.Key, requestHeader.Value);
+            }
+
+            return requestMessage;
+        }
+
+        private T SendRequest<T>(HttpMethod httpMethod, Uri uri, T payload)
+        {
+            try
+            {
+                var requestMessage = new HttpRequestMessage()
+                {
+                    Method = httpMethod,
+                    RequestUri = uri
+                };
+
+                if (RequestHeaders != null)
+                    requestMessage = SetRequestHeaders(requestMessage);
+
+
+                if (payload != null)
+                {
+                    var body = JsonConvert.SerializeObject(payload, Formatting.Indented);
+                    requestMessage.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+                    Response = Client.Send(requestMessage);
+
+                    var responseContent = JsonConvert.DeserializeObject<T>(Response.Content.ReadAsStringAsync().Result);
+
+                    return responseContent;
+                }
+
+                Response = Client.Send(requestMessage);
+
+                // WORK TO DO: Line 79
+                // Remarks: Temporary fix. probably need to remove deserilization from helper and just return the response
+                // which will cause a huge refactor on this code.
+
+                if (httpMethod == HttpMethod.Get || httpMethod == HttpMethod.Put)
+                    return JsonConvert.DeserializeObject<T>(Response.Content.ReadAsStringAsync().Result);
+
+                return payload;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return (T)Activator.CreateInstance(typeof(T));
+            }
+        }
+
+        public T GetRequest<T>(string endPoint, T? body)
+        {
+            return SendRequest(HttpMethod.Get, new Uri($"{_baseUrl}{endPoint}"), body);
+        }
+
+        public T PostRequest<T>(string endPoint, T? body)
+        {
+            return SendRequest(HttpMethod.Post, new Uri($"{_baseUrl}{endPoint}"), body);
+        }
+
+        public T PutRequest<T>(string endPoint, T? body)
+        {
+            return SendRequest(HttpMethod.Put, new Uri($"{_baseUrl}{endPoint}"), body);
+        }
+
+        public string DeleteRequest(string endPoint)
+        {
+            return SendRequest<string>(HttpMethod.Delete, new Uri($"{_baseUrl}{endPoint}"), null);
+        }
+    }
+}
---
+++ FinalProject.WebServiceClients/RestSharp/RestSharpClientHelper.cs
@@ -0,0 +1,96 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.WebServiceClients.RestSharp
+{
+    public class RestSharpClientHelper
+    {
+        public Dictionary<string, string> RequestHeaders { get; set; }
+
+        public Dictionary<string, string> RequestParameters { get; set; }
+
+        public RestResponse Response { get; set; }
+
+        private static RestClient restClient = new RestClient();
+
+        private readonly string baseUrl;
+
+        public RestSharpClientHelper(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public void AddRequestHeaders(string key, string value)
+        {
+            RequestHeaders ??= new Dictionary<string, string>();
+
+            RequestHeaders.Add(key, value);
+        }
+
+        public void AddRequestParameters(string key, string value)
+        {
+            RequestParameters ??= new Dictionary<string, string>();
+
+            RequestParameters.Add(key, value);
+        }
+
+        public async Task<T> GetRequest<T>(string endpoint, T? payload)
+        {
+            return await SendRequest(Method.Get, new Uri($"{baseUrl}{endpoint}"), payload);
+        }
+
+        public async Task<T> PostRequest<T>(string endpoint, T? payload)
+        {
+            return await SendRequest(Method.Post, new Uri($"{baseUrl}{endpoint}"), payload);
+        }
+
+        public async Task<T> PutRequest<T>(string endpoint, T? payload)
+        {
+            return await SendRequest(Method.Put, new Uri($"{baseUrl}{endpoint}"), payload);
+        }
+
+        public async Task<T> PatchRequest<T>(string endpoint, T? payload)
+        {
+            return await SendRequest(Method.Patch, new Uri($"{baseUrl}{endpoint}"), payload);
+        }
+
+        public async Task<RestResponse> DeleteRequest(string endpoint)
+        {
+            return await SendRequest<RestResponse>(Method.Delete, new Uri($"{baseUrl}{endpoint}"), null);
+        }
+
+        private async Task<T> SendRequest<T>(Method method, Uri uri, T? payload)
+        {
+            var request = new RestRequest(uri, method);
+
+            if (RequestHeaders != null)
+                request.AddHeaders(RequestHeaders);
+
+            if (RequestParameters != null)
+                request = AddParameters(request);
+
+            if (payload != null)
+                request.AddBody(payload);
+
+            var response = await restClient.ExecuteAsync<T>(request);
+
+            Response = response;
+
+            return response.Data!;
+        }
+
+        private RestRequest AddParameters(RestRequest restRequest)
+        {
+            foreach (var requestParameters in RequestParameters)
+            {
+                restRequest.AddQueryParameter(requestParameters.Key, requestParameters.Value);
+            }
+
+            return restRequest;
+        }
+    }
+}
